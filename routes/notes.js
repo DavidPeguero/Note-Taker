0c08deefb5738487a367c26c7d1dd21c8b8be03f
David Peguero <david.peguero.cancel@gmail.com>
2023-04-26 17:58:58 -0400

    Can add notes through the notes route

--- routes/notes.js
+++ routes/notes.js
@@ -2,8 +2,27 @@ const note = require('express').Router();
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
 
-note.get('/', (req,res) => {
+note.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 })
+
+note.post('/', (req, res) => {
+    // TODO: Logic for appending data to the db/db.json file
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully 🚀`);
+    } else {
+        res.error('Error in adding Note');
+    }
+});
 
 module.exports = note;
