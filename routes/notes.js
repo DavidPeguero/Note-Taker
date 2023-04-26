3241aa23d6ec2e2dcf018bc45b726ead54fd8c26
David Peguero <david.peguero.cancel@gmail.com>
2023-04-26 19:06:03 -0400

    Can now add notes with an id to them

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,6 @@
 const note = require('express').Router();
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const { json } = require('express');
+const { readAndAppend, readFromFile, writeToFile} = require('../helpers/fsUtils');
 
 
 note.get('/', (req, res) => {
@@ -8,20 +9,42 @@ note.get('/', (req, res) => {
 
 note.post('/', (req, res) => {
     console.log(req.body);
-
     const { title, text } = req.body;
+    readFromFile('./db/db.json').then((data) => {
+        const notes = JSON.parse(data);
+        let id = notes.length + 1;
 
-    if (req.body) {
-        const newNote = {
-            title,
-            text
-        };
-
-        readAndAppend(newNote, './db/db.json');
-        res.json(`Note added successfully 🚀`);
-    } else {
-        res.error('Error in adding Note');
-    }
+        if (req.body) {
+            const newNote = {
+                id,
+                title,
+                text
+            };
+    
+            readAndAppend(newNote, './db/db.json');
+            res.json(`Note added successfully 🚀`);
+        } else {
+            res.error('Error in adding Note');
+        }
+    });
+    
 });
+
+
+note.delete('/:id', (req, res) => {
+    const id = Number(req.params.id);
+    console.log(id);
+    readFromFile('./db/db.json').then((data) => {
+        const newData = JSON.parse(data);
+        console.log(newData)
+        const results = newData.filter((note) => {
+            console.log(note.id)
+            return note.id !== id
+        });
+        console.log(results)
+        writeToFile('./db/db.json', JSON.stringify(newData));
+    })
+});
+
 
 module.exports = note;
--- server.js
+++ server.js
@@ -17,14 +17,14 @@ app.use('/api', api);
 
 
 
+
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
 //Start listening on the port
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
